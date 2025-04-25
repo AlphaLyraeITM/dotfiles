d61798ae8f25170d2e6c25447e8107076915b6c1
AlphaLyraeITM <v083en@gmail.com>
2025-04-25 03:20:32 +0300

    -a vscode, -u i3

--- .config/i3/scripts/setup_display.sh
+++ .config/i3/scripts/setup_display.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xrandr --output DP-1 --mode 3440x1440 --rate 60 && \
-xrandr --output DP-2 --mode 2560x1440 --right-of DP-1 && \
+xrandr --output DP-0 --primary --mode 3440x1440 --rate 60 && \
+xrandr --output DP-2 --mode 2560x1440 --right-of DP-0 && \
 xgamma -gamma 0.9
---
+++ .config/i3/scripts/setup_wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 1s
+feh --bg-fill ~/.config/wallpapers/girlsitting.jpg
